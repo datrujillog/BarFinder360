33d0a8763f6cb0c2da5b96652f53d50bc3872504
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-12 23:40:03 -0500

    Agregado el enrutador de autenticación

---
+++ routes/authRouter.js
@@ -0,0 +1,29 @@
+import express from "express";
+import AuthService from "../services/authService.js";
+
+function authRouter(app) {
+  const router = express.Router();
+
+  //instanciar el servicio
+  const authServi = new AuthService();
+
+  app.use("/api/auth", router);
+
+  router.post("/signup", async (req, res, next) => {
+    try {
+      const body = req.body;
+      const users = await authServi.signup(body);
+      console.log(users);
+      res.status(201).json({
+        message: "User created",
+        users,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  return router;
+}
+
+export default authRouter;
