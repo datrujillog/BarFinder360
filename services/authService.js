543b0ebea1441d9abef027b09181b0e87c06f4ae
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-13 13:37:39 -0500

    Añadir importaciones de jwt y bcrypt, implementar métodos de encriptación y comparación de contraseñas, y agregar método para crear token

--- services/authService.js
+++ services/authService.js
@@ -1,54 +1,64 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 import { PrismaClient } from "@prisma/client";
 import UserService from "./userService.js";
-
-
+import config from "../config/config.js";
 
 class AuthService {
   constructor() {
     this.prisma = new PrismaClient();
     this.userService = new UserService();
-
-    
   }
 
-  async login (data) {
+  async login(data) {
     try {
+      const { email, password } = data;
+
       const user = await this.userService.getByEmail(data.email);
-    
-      if (user.password !== data.password) {
-        throw new Error("Invalid password");
-      }
+
+      await this.#compare(password, user.password);
+
+
       return user;
-      
     } catch (error) {
       throw error;
     }
   }
 
-  async signup (data) {
-    try {
-      const user = await this.prisma.user.create({
-        data: {
-          nombre: data.nombre,
-          apellido: data.apellido,
-          usuario: data.usuario,
-          email: data.email,
-          password: data.password,
-          telefono: data.telefono,
-          NombreNegocio: data.NombreNegocio,
-          tipoNegocio: data.tipoNegocio,
-          rol: data.rol,
-          estado: data.estado,
-        },
-      });
-      return user;
-    } catch (error) {
-      throw error;
-    }  
-    
+  async signup(data) {
+    if (data.password) {
+      data.password = await this.#encrypt(data.password);
+    }
+    const user = await this.prisma.user.create({ data });
+    return user;
   }
 
+  async crearToken(data) {
+    const token = jwt.sign(payload, config.jwtSecret, {
+      expiresIn: "7d",
+    });
+    return token;
+  }
 
+  async #encrypt(string) {
+    try {
+      const salt = await bcrypt.genSalt();
+      const hash = await bcrypt.hash(string, salt);
+
+      return hash;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  async #compare(string, hash) {
+    const match = await bcrypt.compare(string, hash);
+    if (!match) {
+      throw new Error("Invalid password");
+    }
+    return match;
+  }
 }
 
 export default AuthService;
