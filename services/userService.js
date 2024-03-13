731d9fd878b0e5037ac80ee36013349db8dfcf6b
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-12 23:40:25 -0500

    Añadir archivo de servicio de usuario y actualizar .gitignore

---
+++ services/userService.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+
+
+class UserService {
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+
+   async getAllUsers() {
+        try {
+            const users = await this.prisma.user.findMany();
+            return users;
+        } catch (error) {
+            throw error;
+        }
+        
+    
+    
+    }
+}
+
+// module.exports = UserService;
+
+export default UserService;
