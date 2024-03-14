650065c082a7f35b5ae5cf9915b3dcfa7fca9bc5
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-14 08:31:08 -0500

    Actualizar importación de jwt en authValidation.js

--- middleware/authValidation.js
+++ middleware/authValidation.js
@@ -1,7 +1,7 @@
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 
-// const { forbidden} = require("../helpers/sendStatus");
+// const { 404} = require("../helpers/sendStatus");
 
 function authValidation(req, res, next) {
   const bearer = req.headers.authorization;
@@ -14,7 +14,7 @@ function authValidation(req, res, next) {
 
     if (token) {
       try {
-        const decoded = jwt.verify(token, jwtSecret);
+        const decoded = jwt.verify(token, config.jwtSecret);
 
         console.log(decoded);
 
@@ -24,7 +24,7 @@ function authValidation(req, res, next) {
       } catch ({ message, name }) {
         // const message = error.message
         // const name = error.name
-        return res.status(forbidden).json({
+        return res.status(404).json({
           error: true,
           message,
           type: name,
@@ -33,73 +33,14 @@ function authValidation(req, res, next) {
     }
   }
 
-  return res.status(forbidden).json({
+  return res.status(404).json({
     error: true,
     message: "Insufficient permissions",
   });
 }
 
-function adminValidation(req, res, next) {
-  if (req.user.role === "ADMIN") {
-    return next();
-  } else {
-    return res.status(404).json({
-      error: true,
-      message: "Insufficient permissions",
-    });
-  }
-}
-function vendedorValidation(req, res, next) {
-  if (req.user.role === "applicant") {
-    return next();
-  } else {
-    return res.status(forbidden).json({
-      error: true,
-      message: "Insufficient permissions",
-    });
-  }
-}
-function boxValidation(req, res, next) {
-  if (req.user.role === "employer") {
-    return next();
-  } else {
-    return res.status(forbidden).json({
-      error: true,
-      message: "Insufficient permissions",
-    });
-  }
-}
-function coordinatorValidation(req, res, next) {
-  if (req.user.role === "employer" || req.user.role === "admin") {
-    return next();
-  } else {
-    return res.status(forbidden).json({
-      error: true,
-      message: "Insufficient permissions",
-    });
-  }
-}
-
-function authMiddleware(type) {
-  let middlewares;
-  if (type === "VENDEDOR") {
-    middlewares = [authValidation, vendedorValidation];
-  } else if (type === "COODINADOR") {
-    middlewares = [authValidation, coordinatorValidation];
-  } else if (type === "CAJA") {
-    middlewares = [authValidation, boxValidation];
-  } else if (type === "admin") {
-    middlewares = [authValidation, adminValidation];
-  } else {
-    middlewares = [];
-  }
-
-  return middlewares;
-}
-
-module.exports = authMiddleware;
 
 
 
 
-
+export default authValidation;
