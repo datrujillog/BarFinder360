ed405e73e5b2d47c8390345fb7e6c37c86bbbfbf
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-14 19:47:22 -0500

    Añadir importación de PrismaClient y modificar instancias de prisma en authService.js

--- services/authService.js
+++ services/authService.js
@@ -1,20 +1,28 @@
+import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-import { PrismaClient } from "@prisma/client";
 import UserService from "./userService.js";
 import config from "../config/config.js";
 
+// import client from "../helpers/connection.js";
+
+
+
 class AuthService {
+  #client;  
+  #userService ;
   constructor() {
-    this.prisma = new PrismaClient();
-    this.userService = new UserService();
+    // this.#prisma = client;
+    this.#client = new PrismaClient();
+    this.#userService = new UserService();
+
   }
 
   async login(data) {
     try {
       const { email, password } = data;
-      const results = await this.userService.getByEmail(email);
+      const results = await this.#userService.getByEmail(email);
 
       if (!results.success) {
         throw results.error;
@@ -33,7 +41,7 @@ class AuthService {
       if (data.password) {
         data.password = await this.#encrypt(data.password);
       }
-      const user = await this.prisma.user.create({ data });
+      const user = await this.#client.user.create({ data });
       return { success: true, user };
     } catch (error) {
       return { success: false, error };
