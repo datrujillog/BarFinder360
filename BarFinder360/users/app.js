0c3fe794470112999126607205ed92e6d0846489
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-18 14:41:34 -0500

    Agregado archivo app.js con configuraciones y rutas de usuario

---
+++ BarFinder360/users/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+
+import  config  from "./src/configs/config.js";
+
+//importaciones rutas
+import router from "./src/routes/user.rourte.js";
+
+const app = express();
+const port = 5001;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(config.port, () => {
+  console.log("Server is running on port " + config.port);
+  console.log("http://localhost:" + config.port + "/api/");
+});
+
