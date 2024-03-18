2e1b03646a901cadb160ce30bd10bc0be3e6517d
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-18 14:43:24 -0500

    Agregadas rutas de usuario y funciones para obtener, actualizar y eliminar usuarios

---
+++ BarFinder360/users/src/routes/user.rourte.js
@@ -0,0 +1,59 @@
+import express from "express";
+
+import UserService from "../services/users.js";
+
+const router = express.Router();
+
+// INSTANCIAMOS LA CLASE UserService
+const userServ = new UserService();
+
+router.get("/",  async (req, res, next) => {
+    const body = req.body;
+    const results = await userServ.getAllUsers(body);
+    if (!results.success) return res.status(404).json({ error: results.error });
+    return res.status(200).json({ results});
+});
+
+
+
+
+
+router.get("/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const response = await userServ.getUserById(userId);
+    const token = req.headers.authorization.split(" ")[1];
+
+    response.success
+        ? authResponse(res, 200, true, "user found", {
+            payload: response,
+            token: token,
+        })
+        : errorResponse(res, response.error);
+});
+
+router.put("/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const body = req.body;
+    const token = req.headers.authorization.split(" ")[1];
+    const response = await userServ.updateUser(userId, body, token);
+    response.success
+        ? authResponse(res, 200, true, "User ", {
+            payload: response,
+            token: token,
+        })
+        : errorResponse(res, response.error);
+});
+
+router.delete("/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const token = req.headers.authorization.split(" ")[1];
+    const response = await userServ.deleteUser(userId, token);
+    response.success
+        ? authResponse(res, 201, true, "User deleted", {
+            payload: response,
+            token: token,
+        })
+        : errorResponse(res, response.error);
+});
+
+export default router;
