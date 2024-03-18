fa00bb4c3438647a807ca89ebdef4a5ac0df012a
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-18 10:30:27 -0500

    Agregado middleware de autenticación

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
+function auth(req, res, next) {
+  const token = req.cookies.token;
+
+  try {
+    if (!token) {
+      throw new Error("Authentication token not provided.");
+    }
+
+    const decoded = jwt.verify(token, config.jwtSecret);
+    req.user = decoded.results;
+    next();
+  } catch (error) {
+    
+    console.error(error);
+
+    return res.status(403).json({
+      error: true,
+      msg: error.message,
+      message: "Insufficient permissions."
+    });
+  }
+}
+
+export default auth;
