e0e47b59ef91347d78d49d380db6ee961284360e
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-20 22:16:27 -0500

    Agregado el archivo rol.Router.js con la ruta y la función para crear roles de usuario

---
+++ BarFinder360/users/src/routes/rol.Router.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import RoleService from "../services/rol.Service.js";
+import { errorResponse } from "../helpers/response.js";
+
+// const router = express.Router();
+
+function userRouter(app) {
+  const router = express.Router();
+
+  // INSTANCIAMOS LA CLASE UserService
+  const roleServ = new RoleService();
+
+  app.use("/api/users/role", router);
+
+  
+
+  router.post("/create", async (req, res, next) => {
+    const body = req.body;
+    const response = await roleServ.createRole(body);
+    response.success
+      ? res.status(201).json({ message: "Role created", user: response.results })
+      : errorResponse(res, response.error);
+  });
+}
+
+export default userRouter;
