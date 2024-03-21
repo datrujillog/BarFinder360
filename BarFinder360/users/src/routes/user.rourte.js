dd902586a6a8cb8fa90dc1c7b049cb01bfbe777e
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-20 22:16:45 -0500

    Actualizar rutas de usuario y servicios

--- BarFinder360/users/src/routes/user.rourte.js
+++ BarFinder360/users/src/routes/user.rourte.js
@@ -1,78 +1,64 @@
 import express from "express";
 
-import UserService from "../services/users.js";
+import UserService from "../services/users.Service.js";
+import { errorResponse } from "../helpers/response.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-// INSTANCIAMOS LA CLASE UserService
-const userServ = new UserService();
+function userRouter(app) {
+  const router = express.Router();
 
-router.get("/ejemplo", async (req, res, next) => {
-  res.status(200).json({ message: "Hola mundo" });
-});
+  // INSTANCIAMOS LA CLASE UserService
+  const userServ = new UserService();
 
-router.post("/create", async (req, res, next) => {
-  const body = req.body;
-  const response = await userServ.createUser(body);
-  response.success
-    ? authResponse(res, 201, true, "User created", {
-        payload: response,
-        token: response.token,
-      })
-    : errorResponse(res, response.error);
-});
+  app.use("/api/users", router);
 
-router.post("/createRole", async (req, res, next) => {
-  const body = req.body;
-  const response = await userServ.createRole(body);
-  response.success
-    ? authResponse(res, 201, true, "Role created", {
-        payload: response,
-        token: response.token,
-      })
-    : errorResponse(res, response.error);
-});
-
-router.get("/", async (req, res, next) => {
-  const body = req.body;
-  const results = await userServ.getAllUsers(body);
-  if (!results.success) return res.status(404).json({ error: results.error });
-  return res.status(200).json({ results });
-});
-
-router.get("/:id", async (req, res, next) => {
-  const userId = req.params.id;
-  const response = await userServ.getUserById(userId);
-  return res.json({
-    success: response.success,
-    user: response.results,
-  
+  router.post("/create", async (req, res, next) => {
+    const body = req.body;
+    const response = await userServ.createUser(body);
+    response.success
+      ? res
+          .status(201)
+          .json({ message: "User created", user: response.results })
+      : errorResponse(res, response.error);
   });
-});
 
-router.put("/:id", async (req, res, next) => {
-  const userId = req.params.id;
-  const body = req.body;
-  const token = req.headers.authorization.split(" ")[1];
-  const response = await userServ.updateUser(userId, body, token);
-  response.success
-    ? authResponse(res, 200, true, "User ", {
-        payload: response,
-        token: token,
-      })
-    : errorResponse(res, response.error);
-});
+  router.get("/", async (req, res, next) => {
+    const body = req.body;
+    const response = await userServ.getAllUsers(body);
+    if (!response.success)
+      return res.status(404).json({ error: results.error });
+    response.success
+      ? res.status(200).json({ users: response.results })
+      : errorResponse(res, response.error);
+  });
 
-router.delete("/:id", async (req, res, next) => {
-  const userId = req.params.id;
-  const token = req.headers.authorization.split(" ")[1];
-  const response = await userServ.deleteUser(userId, token);
-  response.success
-    ? authResponse(res, 201, true, "User deleted", {
-        payload: response,
-        token: token,
-      })
-    : errorResponse(res, response.error);
-});
+  router.get("/One/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const response = await userServ.getUserById(userId);
+    response.success
+      ? res.status(200).json({ user: response.results })
+      : errorResponse(res, response.error);
+  });
 
-export default router;
+  router.put("/update/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const body = req.body;
+    const response = await userServ.updateUser(userId, body);
+    response.success
+      ? res
+          .status(201)
+          .json({ message: "User updated", user: response.results })
+      : errorResponse(res, response.error);
+  });
+
+  router.delete("/delete/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const response = await userServ.deleteUser(userId);
+    response.success
+      ? res.status(200).json({ message: response.message })
+      : errorResponse(res, response.error);
+  });
+}
+
+export default userRouter;
