97bedcc556323e30f08f4565cbbf02c033d31e08
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-23 00:20:53 -0500

    Add user router to handle user-related API endpoints

---
+++ BarFinder360/users/src/routes/user.router.js
@@ -0,0 +1,64 @@
+import express from "express";
+
+import UserService from "../services/users.Service.js";
+import { errorResponse } from "../helpers/response.js";
+
+// const router = express.Router();
+
+function userRouter(app) {
+  const router = express.Router();
+
+  // INSTANCIAMOS LA CLASE UserService
+  const userServ = new UserService();
+
+  app.use("/api/users", router);
+
+  router.post("/create", async (req, res, next) => {
+    const body = req.body;
+    const response = await userServ.createUser(body);
+    response.success
+      ? res
+          .status(201)
+          .json({ message: "User created", user: response.results })
+      : errorResponse(res, response.error);
+  });
+
+  router.get("/", async (req, res, next) => {
+    const body = req.body;
+    const response = await userServ.getAllUsers(body);
+    if (!response.success)
+      return res.status(404).json({ error: results.error });
+    response.success
+      ? res.status(200).json({ users: response.results })
+      : errorResponse(res, response.error);
+  });
+
+  router.get("/One/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const response = await userServ.getUserById(userId);
+    response.success
+      ? res.status(200).json({ user: response.results })
+      : errorResponse(res, response.error);
+  });
+
+  router.put("/update/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const body = req.body;
+    const response = await userServ.updateUser(userId, body);
+    response.success
+      ? res
+          .status(201)
+          .json({ message: "User updated", user: response.results })
+      : errorResponse(res, response.error);
+  });
+
+  router.delete("/delete/:id", async (req, res, next) => {
+    const userId = req.params.id;
+    const response = await userServ.deleteUser(userId);
+    response.success
+      ? res.status(200).json({ message: response.message })
+      : errorResponse(res, response.error);
+  });
+}
+
+export default userRouter;
