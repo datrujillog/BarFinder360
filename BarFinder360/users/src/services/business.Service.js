82d08cdab41ab31e4ecb1991d92d64f48a10b983
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-24 18:05:57 -0500

    Add random code generation and update role with business ID

--- BarFinder360/users/src/services/business.Service.js
+++ BarFinder360/users/src/services/business.Service.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import RoleService from "./rol.Service.js";
+import randomCode from "../helpers/randomCode.js";
 
 class BusinessService {
   #client;
@@ -13,38 +14,36 @@ class BusinessService {
     try {
       const role = await this.#roleServ.createRole({ name: "ADMIN" });
       const defailRole = role.results.id;
+      const randon = await randomCode();
       const results = await this.#client.business.create({
         data: {
+          code: randon, //await randomCode(),
           ...body,
-          User: {
-            create: {
-              name: body.name,
-              lastName: body.lastName,
-              email: body.email,
-              password: body.password,
-              phone: body.phone,
-              Role: {
-                connect: {
-                  id: defailRole,
-                },
-              },
+          Role: {
+            connect: {
+              id: defailRole,
             },
           },
         },
       });
+
+      //ACTULIZAR LA TABLA DE ROLES CON EL ID DE BUSINESS
+      await this.#roleServ.updateRole({ id: defailRole, BusinessId: results.id });
+
       return { success: true, results };
     } catch (error) {
       return { success: false, error };
     }
   }
 
+  //! VALIDAR ESTE METODO
   async getBusinessById(id) {
     try {
       const results = await this.#client.business.findUnique({
         where: {
           id: parseInt(id),
         },
-      }); 
+      });
       if (!results) {
         const err = new Error("Business not found");
         err.code = 404;
