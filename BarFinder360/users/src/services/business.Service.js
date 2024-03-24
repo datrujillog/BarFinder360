215ffb46572b4aa0adc20de06fd6276fbdfb5099
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-24 12:39:11 -0500

    Add RoleService dependency and connect Role to User in createBusiness method

--- BarFinder360/users/src/services/business.Service.js
+++ BarFinder360/users/src/services/business.Service.js
@@ -1,25 +1,42 @@
 import { PrismaClient } from "@prisma/client";
-
+import RoleService from "./rol.Service.js";
 
 class BusinessService {
   #client;
+  #roleServ;
   constructor() {
     this.#client = new PrismaClient();
+    this.#roleServ = new RoleService();
   }
 
-    async createBusiness(body) {
-        try {
-        const results = await this.#client.business.create({
-            data: {
-            ...body,
+  async createBusiness(body) {
+    try {
+      const role = await this.#roleServ.createRole({ name: "ADMIN" });
+      const defailRole = role.results.id;
+      const results = await this.#client.business.create({
+        data: {
+          ...body,
+          User: {
+            create: {
+              name: body.name,
+              lastName: body.lastName,
+              email: body.email,
+              password: body.password,
+              phone: body.phone,
+              Role: {
+                connect: {
+                  id: defailRole,
+                },
+              },
             },
-        
-        });
-        return { success: true, results };
-        } catch (error) {
-        return { success: false, error };
-        }
+          },
+        },
+      });
+      return { success: true, results };
+    } catch (error) {
+      return { success: false, error };
     }
+  }
 }
 
 export default BusinessService;
