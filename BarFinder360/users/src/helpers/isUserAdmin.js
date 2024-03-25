44426ba0106f8d704211f4f95ea7075b41df58fd
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-24 19:49:15 -0500

    Add isUserAdmin helper function

---
+++ BarFinder360/users/src/helpers/isUserAdmin.js
@@ -0,0 +1,24 @@
+
+import RoleService from "../services/rol.Service.js";
+
+const roleServ = new RoleService();
+
+
+async function isUserAdmin(user) {
+    try {
+        const userRole = await roleServ.getRoleById(user)  ;
+        if (userRole.results.name !== "ADMIN") {
+            throw new Error("User is not an admin");
+        }
+        // Comprobar si el ID de negocio del usuario coincide con el de la solicitud
+        //!El id de la solicitud de la request de la empresa que esta logeada
+        // if (userRole.results.BusinessId !== "El id de la solicitud de la request de la empresa que esta logeada") {
+        //     throw new Error("You are not authorized to create users");
+        // }
+        return { success: true, results: userRole.results};
+    } catch (error) {
+        throw new Error("Failed to check user admin status: " + error.message);
+    }
+}
+
+export default isUserAdmin;
