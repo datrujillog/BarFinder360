e8777f72bcff1e4e24ad054ecd86873155742424
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-25 21:49:58 -0500

    Fix import statement in auth middleware

--- project/middleware/auth.js
+++ project/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import config from "../config/config.js";
+import config from "../configs/config.js";
 
 function auth(req, res, next) {
   const token = req.cookies.token;
