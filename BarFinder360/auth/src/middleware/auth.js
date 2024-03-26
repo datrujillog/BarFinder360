80e66f5d9649cebc27bdc7eec56cf2fe0a2d5ed0
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-26 11:16:48 -0500

    Add auth middleware for authentication

---
+++ BarFinder360/auth/src/middleware/auth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import config from "../configs/config.js";
+
+function auth(req, res, next) {
+  const token = req.cookies.token;
+
+  try {
+    if (!token) {
+      throw new Error("Authentication token not provided.");
+    }
+    const decoded = jwt.verify(token, config.jwtSecret);
+    req.user = decoded.results;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(403).json({
+      error: true,
+      msg: error.message,
+      message: "Insufficient permissions.",
+    });
+  }
+}
+
+export default auth;
