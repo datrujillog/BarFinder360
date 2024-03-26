109ce660dc91cc36dd0f35fa0b73c5d1de12f1f0
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-26 12:02:22 -0500

    Add create and get user business routes

--- BarFinder360/users/src/routes/business.Router.js
+++ BarFinder360/users/src/routes/business.Router.js
@@ -12,13 +12,23 @@ function businessRouter(app) {
   const businessServ = new BusinessService();
 
   app.use("/api/users/business", router);
-  
-// ! se crea desde el micdroservisi de auth con signup
+
+  // ! se crea desde el micdroservisi de auth con signup
   router.post("/create", async (req, res, next) => {
     const body = req.body;
     const response = await businessServ.createBusiness(body);
     response.success
       ? res.status(201).json({ message: "Business created", user: response.results })
+      : errorResponse(res, response.error);
+  });
+
+
+  router.get("/userBusiness/:id", async (req, res, next) => {
+    // const body = req.body;
+    const businessId = req.params.id;
+    const response = await businessServ.userBusiness(businessId);
+    response.success
+      ? res.status(201).json({ message: "Business users", user: response.results })
       : errorResponse(res, response.error);
   });
 }
