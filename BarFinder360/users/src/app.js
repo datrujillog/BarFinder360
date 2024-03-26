4ef534367965f1e4763c77799d9c209513a44ff1
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-26 12:05:33 -0500

    Add CORS configuration to app.js

--- BarFinder360/users/src/app.js
+++ BarFinder360/users/src/app.js
@@ -14,7 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+})); // habilita CORS
 
 
 //use routes
