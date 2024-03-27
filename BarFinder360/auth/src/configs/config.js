f0fbe58c2122b300046fff64c8b5982d64e9f2a9
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-27 00:34:29 -0500

    Update URLs in config.js file

--- BarFinder360/auth/src/configs/config.js
+++ BarFinder360/auth/src/configs/config.js
@@ -9,10 +9,13 @@ import 'dotenv/config'
  const config = {
     jwtSecret: process.env.JW_SECRET || 'secret',
     port: process.env.PORT  || 5002,
-    usersUrl: process.env.USERS_URL || 'http://localhost:5000',
-    urlByEmail: process.env.URL_BY_EMAIL || 'http://localhost:5001/api/users/ByEmail',
-    urlSignup: process.env.URL_SIGNUP || 'http://localhost:5001/api/users/business/create',
+   //  usersUrl: process.env.USERS_URL || 'http://localhost:5000',
+    urlByEmail: process.env.URL_BY_EMAIL || 'http://10.96.214.1433:5001/api/users/ByEmail',
+    urlSignup: process.env.URL_SIGNUP || 'http://10.96.214.143:5001/api/users/business/create',
    
  };
 
  export default config;
+
+//  urlByEmail: process.env.URL_BY_EMAIL || 'http://localhost:5001/api/users/ByEmail',
+//     urlSignup: process.env.URL_SIGNUP || 'http://localhost:5001/api/users/business/create',
