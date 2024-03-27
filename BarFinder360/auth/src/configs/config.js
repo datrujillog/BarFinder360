e6b1ed9d84bc0c1d0efd92f0af48a35ac8bac6a7
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-26 23:08:40 -0500

    Update port number in config.js

--- BarFinder360/auth/src/configs/config.js
+++ BarFinder360/auth/src/configs/config.js
@@ -8,7 +8,7 @@ import 'dotenv/config'
 
  const config = {
     jwtSecret: process.env.JW_SECRET || 'secret',
-    port: process.env.PORT  || 5000,
+    port: process.env.PORT  || 5002,
     usersUrl: process.env.USERS_URL || 'http://localhost:5000',
    
  };
